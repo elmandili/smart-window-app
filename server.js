6e3b2ce2dcc9662289bf5e437629977d69de023e
elmandili <elmandilimail@gmail.com>
2025-04-26 15:20:36 +0100

    modify server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Use the environment variable PORT or default to 3000
+
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 // Set EJS as the view engine
